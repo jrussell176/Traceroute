18cb36076e5b7eb4a641702abd26cb72475ffe34
Jared Russell <Jared Russell>
2017-04-28 15:41:55 -0500

    Adding starting files for traceroute

---
+++ Traceroute/Header.h
@@ -0,0 +1,23 @@
+#pragma once
+
+/* Modified header file from HW3*/
+#include <stdlib.h>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <time.h>
+#include <stack>
+#include <iostream> 
+#include <fstream>
+#include <stdio.h>
+#include <unordered_set>
+#include <strsafe.h>
+#include <tchar.h>
+#include <crtdbg.h>
+#include <map>
+
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <Windows.h>
+
+#pragma comment(lib, "Ws2_32.lib")
